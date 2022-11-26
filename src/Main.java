dbb976a77c9912db6f14f8ef126dca1e074696e8
OneSupArmy <saska.jukoff@gmail.com>
2022-11-26 11:41:01 +0300

    Homework - corrected verion

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,16 @@ public class Main {
         int savePart = 15000;
         int total = 0;
         int month = 1;
+        double percent = 0.12;
         while (total < 2_459_000) {
-            total = (int) ((total + savePart) * 1.12);//Копейки на благотворительность
+            if (month % 12 == 0) {
+                total += total * 0.12;
+            }
+            total += savePart;
             System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
             month++;
         }
+        System.out.println("");
         //Part 1 - Task 2
         int i = 0;
         while (i < 10) {
@@ -21,19 +26,17 @@ public class Main {
         for (; i >= 1; i--) {
             System.out.print(i + " ");
         }
-        System.out.println("");
+        System.out.println("\n");
         //Part 1 - Task 3
         int population = 12_000_000;
         byte fertility = 17;
         byte mortality = 8;
-        /*Можно конечно считать с их разницей, но так если потребуется понять сколько рождалось каждый
-        месяц, а сколько умирало это можно будет вывести.*/
-        int fertilityOfPopulation = (population / 1000) * fertility;
-        int mortalityOfPopulation = (population / 1000) * mortality;
+        int increaseCapacity = fertility - mortality;
         for (int l = 1; l <= 10; l++) {
-            population = population - mortalityOfPopulation + fertilityOfPopulation;
+            population += population/1000*increaseCapacity;
             System.out.println("Год " + l + ", численность населения составляет " + population);
         }
+        System.out.println("");
         task21();
         task22();
         task23();
@@ -41,13 +44,13 @@ public class Main {
         task31();
         task32();
     }
-    final static float DEPOSIT = 1.07f;
-    final static int VASYA_ACCUM = 15000;
+    public final static float DEPOSIT = 1.07f;
+    public final static int VASYA_ACCUM = 15000;
     public static void task21() {
         //Part 2 - Task 1
-        int totalVasyaSave = 0;
+        int totalVasyaSave = 15000;
         for (int g = 1; totalVasyaSave <= 12_000_000; g++) {
-            totalVasyaSave = (int) ((totalVasyaSave + VASYA_ACCUM) * DEPOSIT);//Копейки в подарок банку, Вася же миллионер
+            totalVasyaSave *= DEPOSIT;
             System.out.println("На счету у Васи уже " + totalVasyaSave);
             if (totalVasyaSave >= 12_000_000)
                 System.out.println("Поздравляем! Количество месяцев потребовавшееся Васе = " + g);
@@ -56,9 +59,9 @@ public class Main {
     }
     public static void task22() {
         //Part 2 - Task 2
-        int totalVasyaSave = 0;
+        int totalVasyaSave = 15000;
         for (int g = 1; totalVasyaSave <= 12_000_000; g++) {
-            totalVasyaSave = (int) ((totalVasyaSave + VASYA_ACCUM) * DEPOSIT);//Копейки в подарок банку, Вася же миллионер
+            totalVasyaSave *= DEPOSIT;
             if (g % 6 == 0) {
                 System.out.println("На счету у Васи уже " + totalVasyaSave);
             }
@@ -90,15 +93,19 @@ public class Main {
             System.out.println("Сегодня пятница " + firstFriday + "-е число. Необходимо подготовить отчет.");
             firstFriday += 7;
          */
+        System.out.println("");
     }
     public static void task31() {
         //Part 3 - Task 1
+        Scanner scYear = new Scanner(System.in);
+        System.out.println("Input actual year:");
         int periodOfCometComing = 79;
-        int yearNow = 2022;
+        int yearNow = scYear.nextInt();
+        int startYear = yearNow - 200;
         int yearOfComing = 0;
         do {
             yearOfComing += periodOfCometComing;
-            if (yearOfComing > (yearNow - 200)) {
+            if (yearOfComing > startYear) {
                 System.out.println(yearOfComing);
             }
         } while (yearOfComing < yearNow);
